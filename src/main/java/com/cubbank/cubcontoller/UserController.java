927488a0be3b70cae6f383971cf90ab7e0482d46
Osama Khan <osamakhanjmi@gmail.com>
2017-09-19 13:36:40 +0530

    OK "final" Commit

--- src/main/java/com/cubbank/cubcontoller/UserController.java
+++ src/main/java/com/cubbank/cubcontoller/UserController.java
@@ -1,13 +1,20 @@
 package com.cubbank.cubcontoller;
 
+import com.sun.deploy.net.HttpRequest;
 import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
 import com.cubbank.cubentity.User;
 import com.cubbank.service.UserService;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -20,19 +27,24 @@ public class UserController {
     }
 
     @RequestMapping(path="/login", method = RequestMethod.POST)
-    public @ResponseBody void validateUserLogin(@RequestParam("userId") String userId, @RequestParam("password") String pswd){
-        User user = userService.getUserById(userId);
-        if ( user.getUserId().equals(userId)) {
+    public ModelAndView validateUserLogin(ModelAndView modelAndView, @Valid User user, BindingResult bindingResult,
+                                          @RequestParam Map requestParams, RedirectAttributes rdrAttr){
 
-            if(user.getUserPassword().equals(pswd)){
-
-            } else{
-
-                if(user.incrementLoginAttempts()<3){
-                    
+        modelAndView.setViewName("");
+        if(userService.getUserById(user.getUserId())!=null) {
+            User usr = userService.getUserById(user.getUserId());
+            if(user.getUserPassword().equals(usr.getUserPassword())){
+                if(user.getLoginAttempts()<3){
+                    modelAndView.setViewName("");
+                }else{
+                        rdrAttr.addFlashAttribute("errorMessage","You are blocked");
                 }
+            }else{
+                rdrAttr.addFlashAttribute("errorMessage","Password is wrong");
             }
+        }else{
+            rdrAttr.addFlashAttribute("errorMessage","Wrong User Id");
         }
+        return modelAndView;
     }
-
 }
