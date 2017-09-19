863b75757fbb5039d581cbb8f3b4133fe0138dd2
Osama Khan <osamakhanjmi@gmail.com>
2017-09-19 14:07:57 +0530

    OK "final" Commit

--- src/main/java/com/cubbank/cubentity/CustomerBenificiary.java
+++ src/main/java/com/cubbank/cubentity/CustomerBenificiary.java
@@ -8,4 +8,36 @@ public class CustomerBenificiary extends Customer{
     private String benificiaryAccountName;
     private String benificiaryBankIFSC;
     private String benificiaryAccountStatus;
+
+    public int getBenificiaryAccountNo() {
+        return benificiaryAccountNo;
+    }
+
+    public void setBenificiaryAccountNo(int benificiaryAccountNo) {
+        this.benificiaryAccountNo = benificiaryAccountNo;
+    }
+
+    public String getBenificiaryAccountName() {
+        return benificiaryAccountName;
+    }
+
+    public void setBenificiaryAccountName(String benificiaryAccountName) {
+        this.benificiaryAccountName = benificiaryAccountName;
+    }
+
+    public String getBenificiaryBankIFSC() {
+        return benificiaryBankIFSC;
+    }
+
+    public void setBenificiaryBankIFSC(String benificiaryBankIFSC) {
+        this.benificiaryBankIFSC = benificiaryBankIFSC;
+    }
+
+    public String getBenificiaryAccountStatus() {
+        return benificiaryAccountStatus;
+    }
+
+    public void setBenificiaryAccountStatus(String benificiaryAccountStatus) {
+        this.benificiaryAccountStatus = benificiaryAccountStatus;
+    }
 }
